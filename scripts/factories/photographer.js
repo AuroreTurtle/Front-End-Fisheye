3e7e4f98d249f180079eefeb9a9bbb4c5961c3ba
Aurore GOURRAUD <aurore.gourraud@gmail.com>
2022-04-15 18:19:41 +0200

    Home finis : récupération des données json pour les afficher + lien vers la page des photographes

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,41 @@
+/* Creating a function that will create a photographer object. */
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { id, name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+      const article = document.createElement("article");
+
+      const a = document.createElement("a");
+      a.setAttribute("href", `photographer.html?id=${id}`);
+
+      const img = document.createElement("img");
+      img.setAttribute("src", picture);
+      img.setAttribute("alt", name);
+
+      const h2 = document.createElement("h2");
+      h2.textContent = name;
+
+      const h3 = document.createElement("h3");
+      h3.textContent = `${city}, ${country}`;
+
+      const pTag = document.createElement("p");
+      pTag.className = "tag";
+      pTag.textContent = tagline;
+
+      const pPrix = document.createElement("p");
+      pPrix.className = "prix";
+      pPrix.textContent = `${price}€/jour`;
+
+      article.appendChild(a);
+      a.appendChild(img);
+      a.appendChild(h2);
+      article.appendChild(h3);
+      article.appendChild(pTag);
+      article.appendChild(pPrix);
+      return article;
     }
-    return { name, picture, getUserCardDOM }
+
+    return { id, name, picture, getUserCardDOM };
 }
